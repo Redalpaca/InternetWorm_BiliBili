3b64f029dafcd803980c7d4be3040f0fab75caf3
Alpaca <1659998742@qq.com>
2022-08-17 13:32:56 +0800

    更新日志: 增加了获取b站搜索结果的功能

--- BiliUserClass.py
+++ BiliUserClass.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 import json
 import os
 from BiliClass import BiliVideo
-from BiliClass_func import DetailedListDownload
+
 
 
 BiliUniHeaders = {
 'authority':'www.bilibili.com',
 'method':'GET',
 'scheme':'https',
-'cookie':'buvid3=C6FA7D37-BDAB-8A98-F46C-F74DCCD9E1E590619infoc;_uuid=611AEF8F-FA10A-F25E-63B1-7C6851A3E69C92883infoc;buvid4=A0BF7CAD-00E0-291D-0057-8C4CC946B8DF91548-022012618-yPTK2yRnbGG1AbZfVzPX2Q%3D%3D;rpdid=|(kRJkkRmJ~0J\'uYRJumJumJ;fingerprint=163f019b63a8ac7654d7e8634c3127cf;buvid_fp_plain=undefined;buvid_fp=4d18731a5a709bbdb5ce9d474ce68827;SESSDATA=201e1f7f%2C1665046259%2C56fc0%2A41;bili_jct=64787cb69067364c5e44cd1c3dd05938;DedeUserID=35671002;DedeUserID__ckMd5=d69f732e9248e565;sid=86748t3l;CURRENT_BLACKGAP=0;blackside_state=0;i-wanna-go-back=-1;b_ut=5;LIVE_BUVID=AUTO9316494943737456;is-2022-channel=1;nostalgia_conf=-1;PVID=1;hit-dyn-v2=1;bsource=search_baidu;bp_video_offset_35671002=683780023791910900;theme_style=light;innersign=1;CURRENT_FNVAL=4048;b_lsid=FDB9A836_1821F9A3573;b_timer=%7B%22ffp%22%3A%7B%22333.1007.fp.risk_C6FA7D37%22%3A%221820B1D4B31%22%2C%22444.41.fp.risk_C6FA7D37%22%3A%221820B1D7971%22%2C%22333.788.fp.risk_C6FA7D37%22%3A%221821FC361D3%22%2C%22333.337.fp.risk_C6FA7D37%22%3A%221820B1F58D8%22%7D%7D',
+'cookie':'buvid3=C6FA7D37-BDAB-8A98-F46C-F74DCCD9E1E590619infoc; _uuid=611AEF8F-FA10A-F25E-63B1-7C6851A3E69C92883infoc; buvid4=A0BF7CAD-00E0-291D-0057-8C4CC946B8DF91548-022012618-yPTK2yRnbGG1AbZfVzPX2Q%3D%3D; rpdid=|(kRJkkRmJ~0J\'uYRJumJumJ; buvid_fp_plain=undefined; blackside_state=0; CURRENT_BLACKGAP=0; i-wanna-go-back=-1; b_ut=5; LIVE_BUVID=AUTO9316494943737456; is-2022-channel=1; nostalgia_conf=-1; hit-dyn-v2=1; go_old_video=-1; theme_style=light; bsource=search_baidu; fingerprint3=b91abfafa0dabad6a036f1b9b0f4c5f3; fingerprint=e5690504a7252e22c88daa209cce9049; CURRENT_FNVAL=4048; b_lsid=B49FD875_182861848B2; b_timer=%7B%22ffp%22%3A%7B%22333.788.fp.risk_C6FA7D37%22%3A%221828618A9AA%22%2C%22333.1193.fp.risk_C6FA7D37%22%3A%221828189D702%22%2C%22333.999.fp.risk_C6FA7D37%22%3A%2218286187503%22%2C%22888.2421.fp.risk_C6FA7D37%22%3A%221828193B951%22%2C%22666.25.fp.risk_C6FA7D37%22%3A%22182859DCF3D%22%2C%22333.976.fp.risk_C6FA7D37%22%3A%2218272FE2BEE%22%2C%22444.41.fp.risk_C6FA7D37%22%3A%221827BA2EEE0%22%2C%22333.937.fp.risk_C6FA7D37%22%3A%22182731D8041%22%2C%22333.337.fp.risk_C6FA7D37%22%3A%221828596C0EF%22%2C%22777.5.0.0.fp.risk_C6FA7D37%22%3A%221828193925D%22%2C%22666.19.fp.risk_C6FA7D37%22%3A%2218276C82C19%22%2C%22333.967.fp.risk_C6FA7D37%22%3A%22182818AB45E%22%2C%22333.880.fp.risk_C6FA7D37%22%3A%221828193B897%22%2C%22333.42.fp.risk_C6FA7D37%22%3A%22182861C7407%22%7D%7D; SESSDATA=e20c69d7%2C1675659396%2C09715%2A81; bili_jct=54992fbb96d8876b46cc8abb37df1a57; DedeUserID=35671002; DedeUserID__ckMd5=d69f732e9248e565; buvid_fp=e5690504a7252e22c88daa209cce9049; CURRENT_QUALITY=116; bp_video_offset_35671002=692641245482188900; sid=7p2ab96c; PVID=11',
 'referer':'www.bilibili.com',
 'sec-ch-ua-platform':'"Windows"',
 'sec-fetch-dest':'document',
@@ -36,11 +36,11 @@ BiliApiHeaders = {
     'user-agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/103.0.0.0Safari/537.36'
 }
 
-class Person(object):
+class BiliUser(object):
     SpaceURL_mode = 'https://space.bilibili.com/{userID}'
     def __init__(self, userID):
         self.userID = userID
-        self.SpaceURL = Person.SpaceURL_mode.format(userID = userID)
+        self.SpaceURL = BiliUser.SpaceURL_mode.format(userID = userID)
         
         self.res = requests.get(self.SpaceURL, headers= BiliUniHeaders)
         self.name = re.search('title>(.*?)的个人空间_', self.res.text).group(1)
@@ -83,15 +83,84 @@ class Person(object):
                 vlist.append(element)
             pass
         return vlist
+    #获取基本信息
+    def TotalInfo(self):
     
+        pass
     
+#总体BiliBili类, 用于广泛查询
+class BiliBili(object):
+    origin_url = 'https://www.bilibili.com/'
+    #频道字典
+    channel = {'番剧': 'https://www.bilibili.com/anime/', '电影': 'https://www.bilibili.com/movie/', '国创': 'https://www.bilibili.com/guochuang/', '电视剧': 'https://www.bilibili.com/tv/', '综艺': 'https://www.bilibili.com/variety/ ', '纪录片': 'https://www.bilibili.com/documentary/', '动画': 'https://www.bilibili.com/v/douga/', '游戏': 'https://www.bilibili.com/v/game/', '鬼畜': 'https://www.bilibili.com/v/kichiku/', '音乐': 'https://www.bilibili.com/v/music', '舞蹈': 'https://www.bilibili.com/v/dance/', '影视': 'https://www.bilibili.com/v/cinephile', '娱乐': 'https://www.bilibili.com/v/ent/', '知识': 'https://www.bilibili.com/v/knowledge/', '科技': 'https://www.bilibili.com/v/tech/', '资讯': 'https://www.bilibili.com/v/information/', '美食': 'https://www.bilibili.com/v/food', '生活': 'https://www.bilibili.com/v/life', '汽车': 'https://www.bilibili.com/v/car', '时尚': 'https://www.bilibili.com/v/fashion', '运动': 'https://www.bilibili.com/v/sports', '动物圈': 'https://www.bilibili.com/v/animal', 'VLOG': 'https://www.bilibili.com/v/life/daily/#/530003', '搞笑': 'https://www.bilibili.com/v/life/funny', '单机游戏': 'https://www.bilibili.com/v/game/stand_alone', '虚拟UP主': 'https://www.bilibili.com/v/virtual', '公益': 'https://love.bilibili.com', '公开课': 'https://www.bilibili.com/mooc'}
     
+    def __init__(self):
+        self.res = requests.get(BiliBili.origin_url, headers= BiliUniHeaders)
+        self.html = self.res.text
+        pass    
+    #如果频道有变化则进行更新
+    def channel_update(self):
+        soup = BeautifulSoup(self.html, 'lxml')
+        NodeList = soup.find_all('a', class_ = 'channel-link')
+        dict_0 = {}
+        for node in NodeList:
+            dict_0[node.string] = node.attrs['href']
+        print(dict_0)
+        BiliBili.channel = dict_0
+        pass
+    #使用api简易搜索单独的页面
+    def search_single(self, keyword = '1', mode = 'comprehensive', page = 1):
+        dict_order = {'comprehensive':' ', 'new':'pubdate', 'view':'click', 'danmu':'dm', 'bookmark':'stow'}
+        try:
+            order = dict_order[mode]
+        except:
+            order = dict_order['bookmark']
+        keyword = keyword.replace(' ', '%20')
+        #api页面的显示方式是: 一页20个视频, 若能返回搜索结果, 则最多有1000个. 
+        apiURL_mode = 'https://api.bilibili.com/x/web-interface/search/all/v2?__refresh__=true&_extra=&context=&page={page}&order={order}&duration=&platform=pc&highlight=1&single_column=0&keyword={keyword}&preload=true&com2co=true'
+        apiURL = apiURL_mode.format(order= order, keyword= keyword, page = page)
+        res = requests.get(apiURL, headers= BiliApiHeaders)
+        dict_0 = json.loads(res.text)
+        numPages = dict_0['data']['numPages']
+        for element in dict_0['data']['result']:
+            if element['result_type'] == 'video':
+                VideoDict = element
+                break
+            else: VideoDict = {}
+        bvList = []
+        for video in VideoDict['data']:
+            bvList.append(video['bvid'])
+        #print(bvList.__len__())
+        return bvList    
+    #使用api直接获取从第一页到指定页的搜索结果
+    #警告: 多次使用该方法容易报412错误, 建议使用selenium等测试软件进行爬虫
+    def search_all(self, keyword = '1', mode = 'comprehensive', page_end = 1):
+        #获取总页数
+        apiURL = 'https://api.bilibili.com/x/web-interface/search/all/v2?__refresh__=true&_extra=&context=&page=1&order=&duration=&platform=pc&highlight=1&single_column=0&keyword={keyword}&preload=true&com2co=true'.format(keyword= keyword)
+        res = requests.get(apiURL, headers= BiliApiHeaders)
+        numPages = json.loads(res.text)['data']['numPages']
+        page_end = page_end if numPages > page_end else numPages
+        #搜索并添加
+        bvList_all = []
+        for page in range(1,page_end+1):
+            print('Getting page: %d.'%page)
+            temp = self.search_single(keyword= keyword, page= page, mode= mode)
+            bvList_all.extend(temp)
+        print(len(bvList_all))
+        return bvList_all
+        pass
+    #缓慢搜索
+    def search_safe(self, keyword = '1'):
+        
+        pass
     
+    pass
 
-alpaca = Person('26607148')
-bvlist = alpaca.VideoBVList()
-print('bvList Created')
-#DetailedListDownload(bvlist)
+    
+site = BiliBili()
+site.search_all(page_end= 1)
+
+
 
 
 
