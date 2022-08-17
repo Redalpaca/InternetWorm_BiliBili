b572b784571aa789f4cfb76d957470b1ec4499f4
Alpaca <1659998742@qq.com>
2022-08-17 16:49:12 +0800

    更新日志: 测试使用selenium进行搜索防止被ban

--- BiliUserClass.py
+++ BiliUserClass.py
@@ -1,11 +1,13 @@
+from email import header
 import requests
 import re
 from bs4 import BeautifulSoup
 import json
 import os
 from BiliClass import BiliVideo
-
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 BiliUniHeaders = {
 'authority':'www.bilibili.com',
@@ -35,7 +37,37 @@ BiliApiHeaders = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'user-agent':'Mozilla/5.0(WindowsNT10.0;Win64;x64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/103.0.0.0Safari/537.36'
 }
-
+Bilicookies = {
+        "name":"name",
+        "value":"bilibili",
+        "buvid3":"C6FA7D37-BDAB-8A98-F46C-F74DCCD9E1E590619infoc",
+        "_uuid":"611AEF8F-FA10A-F25E-63B1-7C6851A3E69C92883infoc",
+        "buvid4":"A0BF7CAD-00E0-291D-0057-8C4CC946B8DF91548-022012618-yPTK2yRnbGG1AbZfVzPX2Q%3D%3D",
+        "rpdid":"|(kRJkkRmJ~0J\'uYRJumJumJ",
+        "fingerprint":"163f019b63a8ac7654d7e8634c3127cf",
+        "buvid_fp_plain":"undefined",
+        "buvid_fp":"4d18731a5a709bbdb5ce9d474ce68827",
+        "SESSDATA":"201e1f7f%2C1665046259%2C56fc0%2A41",
+        "bili_jct":"64787cb69067364c5e44cd1c3dd05938",
+        "DedeUserID":"35671002",
+        "DedeUserID__ckMd5":"d69f732e9248e565",
+        "sid":"86748t3l",
+        "CURRENT_BLACKGAP":"0",
+        "blackside_state":"0",
+        "i-wanna-go-back":"-1",
+        "b_ut":"5",
+        "LIVE_BUVID":"AUTO9316494943737456",
+        "is-2022-channel":"1",
+        "nostalgia_conf":"-1",
+        "hit-dyn-v2":"1", 
+        "go_old_video":"-1",
+        "CURRENT_FNVAL":"4048",
+        "CURRENT_QUALITY":"120",
+        "bsource":"search_baidu",
+        "bp_video_offset_35671002":"691280144785997800",
+        "b_lsid":"CBD22374_1827669CDBD",
+        "PVID":"2", 
+        "b_timer":"%7B%22ffp%22%3A%7B%22333.788.fp.risk_C6FA7D37%22%3A%2218272FEB16C%22%2C%22333.1193.fp.risk_C6FA7D37%22%3A%221827676EE21%22%2C%22333.999.fp.risk_C6FA7D37%22%3A%221827676EBCC%22%2C%22888.2421.fp.risk_C6FA7D37%22%3A%221826EA1E547%22%2C%22666.25.fp.risk_C6FA7D37%22%3A%221827674CE10%22%2C%22333.976.fp.risk_C6FA7D37%22%3A%2218272FE2BEE%22%2C%22444.41.fp.risk_C6FA7D37%22%3A%2218272FE6D5D%22%2C%22333.937.fp.risk_C6FA7D37%22%3A%22182731D8041%22%2C%22333.337.fp.risk_C6FA7D37%22%3A%221827676F357%22%7D%7D"}
 class BiliUser(object):
     SpaceURL_mode = 'https://space.bilibili.com/{userID}'
     def __init__(self, userID):
@@ -92,8 +124,8 @@ class BiliUser(object):
 class BiliBili(object):
     origin_url = 'https://www.bilibili.com/'
     #频道字典
-    channel = {'番剧': 'https://www.bilibili.com/anime/', '电影': 'https://www.bilibili.com/movie/', '国创': 'https://www.bilibili.com/guochuang/', '电视剧': 'https://www.bilibili.com/tv/', '综艺': 'https://www.bilibili.com/variety/ ', '纪录片': 'https://www.bilibili.com/documentary/', '动画': 'https://www.bilibili.com/v/douga/', '游戏': 'https://www.bilibili.com/v/game/', '鬼畜': 'https://www.bilibili.com/v/kichiku/', '音乐': 'https://www.bilibili.com/v/music', '舞蹈': 'https://www.bilibili.com/v/dance/', '影视': 'https://www.bilibili.com/v/cinephile', '娱乐': 'https://www.bilibili.com/v/ent/', '知识': 'https://www.bilibili.com/v/knowledge/', '科技': 'https://www.bilibili.com/v/tech/', '资讯': 'https://www.bilibili.com/v/information/', '美食': 'https://www.bilibili.com/v/food', '生活': 'https://www.bilibili.com/v/life', '汽车': 'https://www.bilibili.com/v/car', '时尚': 'https://www.bilibili.com/v/fashion', '运动': 'https://www.bilibili.com/v/sports', '动物圈': 'https://www.bilibili.com/v/animal', 'VLOG': 'https://www.bilibili.com/v/life/daily/#/530003', '搞笑': 'https://www.bilibili.com/v/life/funny', '单机游戏': 'https://www.bilibili.com/v/game/stand_alone', '虚拟UP主': 'https://www.bilibili.com/v/virtual', '公益': 'https://love.bilibili.com', '公开课': 'https://www.bilibili.com/mooc'}
-    
+    channel = {'番剧': 'https://www.bilibili.com/anime/', '电影': 'https://www.bilibili.com/movie/', '国创': 'https://www.bilibili.com/guochuang/', '电视剧': 'https://www.bilibili.com/tv/', '综艺': 'https://www.bilibili.com/variety/ ', '纪录片': 'https://www.bilibili.com/documentary/', '动画': 'https://www.bilibili.com/v/douga/', '游戏': 'https://www.bilibili.com/v/game/', '鬼畜': 'https://www.bilibili.com/v/kichiku/', '音乐': 'https://www.bilibili.com/v/music', '舞蹈': 'https://www.bilibili.com/v/dance/', '影视': 'https://www.bilibili.com/v/cinephile', '娱乐': 'https://www.bilibili.com/v/ent/', '知识': 'https://www.bilibili.com/v/knowledge/', '科技': 'https://www.bilibili.com/v/tech/', '资讯': 'https://www.bilibili.com/v/information/', '美食': 'https://www.bilibili.com/v/food', '生活': 'https://www.bilibili.com/v/life', '汽车': 'https://www.bilibili.com/v/car', '时尚': 'https://www.bilibili.com/v/fashion', '运动': 'https://www.bilibili.com/v/sports', '动物圈': 'https://www.bilibili.com/v/animal', 'VLOG': 'https://www.bilibili.com/v/life/daily/#/530003', '搞笑': 'https://www.bilibili.com/v/life/funny', '单机游戏': 'https://www.bilibili.com/v/game/stand_alone', '虚拟UP主': 'https://www.bilibili.com/v/virtual', '公益': 'https://love.bilibili.com', '公开课': 'https://www.bilibili.com/mooc'} 
+    #类构造方法
     def __init__(self):
         self.res = requests.get(BiliBili.origin_url, headers= BiliUniHeaders)
         self.html = self.res.text
@@ -151,16 +183,32 @@ class BiliBili(object):
         pass
     #缓慢搜索
     def search_safe(self, keyword = '1'):
-        
+        url_search = 'https://search.bilibili.com/all?'
+        '<input class="search-input-el" type="text" placeholder="输入关键字搜索" data-v-07fdfa75="">'
+        '<button class="vui_button vui_button--blue vui_button--lg search-button" data-v-07fdfa75="">搜索</button>'
+        browser = webdriver.Chrome()
+        #browser.add_cookie(Bilicookies)
+        browser.get(url_search)
+        InputNode = browser.find_element(By.CLASS_NAME, 'search-input-el')
+        InputNode.send_keys(keyword)
+        #InputNode.submit()
+        button = browser.find_element(By.XPATH , '//*[@id="i_cecream"]/div[1]/div[1]/div/div/div/div/button')
+        button.click()
+        time.sleep(5)
+        '//*[@id="i_cecream"]/div[1]/div[1]/div[2]/div/div/div[1]/div'
+        #AllVideoNode = browser.find_element(By.XPATH, '//*[@id="i_cecream"]/div[1]/div[1]/div[2]/div/div/div[1]/div')
+        '/html/body/div[3]/div[1]/div[1]/div[2]/div/div'
+        AllVideoNode = browser.find_element(By.CSS_SELECTOR, 'body')
+        text = AllVideoNode
+        print(text)
+        time.sleep(50)
+        browser.close()
         pass
     
     pass
 
-    
+
 site = BiliBili()
-site.search_all(page_end= 1)
-
-
 
 
 
